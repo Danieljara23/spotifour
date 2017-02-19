fd5f094488270f62d13f17371cdda2fabb002776
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-18 19:55:59 -0500

    Format of released_at field on Album table is fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219000518) do
+ActiveRecord::Schema.define(version: 20170219004748) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.text     "image_url"
-    t.datetime "released_at"
+    t.date     "released_at"
     t.integer  "artist_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
--- test/fixtures/albums.yml
+++ test/fixtures/albums.yml
@@ -5,7 +5,7 @@
 #  id          :integer          not null, primary key
 #  name        :string
 #  image_url   :text
-#  released_at :datetime
+#  released_at :date
 #  artist_id   :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
